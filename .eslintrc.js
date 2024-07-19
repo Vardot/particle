5279d0d90121e8c8c3b2a5c5bbe9dcc1d99cc9fb
Lisa Ridley <l.ridley@vardot.com>
2024-07-19 10:24:29 -0400

    UTC-32: ignoring linting rules that are generating noise during the theme build process

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,17 @@ module.exports = {
   extends: [
     "airbnb-base",
     "plugin:prettier/recommended",
+    "plugin:yml/recommended",
     "prettier",
   ],
   plugins: ["prettier"],
   root: true,
   globals: {
+    drupalTranslations: true,
+    once: true,
+    tabbable: true,
     Drupal: true,
+    drupalSettings: true,
     jQuery: true,
     _: true,
     BUILD_TARGET: true,
@@ -27,5 +32,25 @@ module.exports = {
     eqeqeq: "error",
     "no-console": [0], // turned off for now while we are console.logging everywhere.
     "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
-  },
+    "no-plusplus": ["warn", {
+      "allowForLoopAfterthoughts": true
+    }],
+    "no-param-reassign": ["off"],
+    "no-prototype-builtins": ["off"],
+    "valid-jsdoc": ["warn", {
+      "prefer": {
+        "returns": "return",
+        "property": "prop"
+      },
+      "requireReturn": false
+    }],
+    "no-unused-vars": ["off"],
+    "prefer-const": ["off"],
+    "object-shorthand": ["off"],
+    "no-inner-declarations": ["off"],
+    "func-names": ["off"],
+    "prefer-template": ["off"],
+    "operator-linebreak": ["error", "after", { "overrides": { "?": "ignore", ":": "ignore" } }],
+    "yml/indent": ["error", 2] 
+  }
 };
