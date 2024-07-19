778f0b8662a769ee731eff941bd86c4cd6ca92d4
Lisa Ridley <l.ridley@vardot.com>
2024-07-19 10:25:36 -0400

    UTC-32: Fixing javascript function location so that it is defined before it is called

--- particle_theme/js/back-to-top-button.js
+++ particle_theme/js/back-to-top-button.js
@@ -5,12 +5,6 @@
       // Get the button:
       const scrollButton = document.getElementById("scroll-to-top-btn");
       if (scrollButton) {
-        scrollButton.style.display = "none";
-
-        // When the user scrolls down 1500px from the top of the document, show the button
-        window.onscroll = function () {
-          scrollFunction();
-        };
         function scrollFunction() {
           if (
             document.body.scrollTop > 1500 ||
@@ -26,6 +20,12 @@
           document.body.scrollTop = 0; // For Safari
           document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
         }
+        scrollButton.style.display = "none";
+
+        // When the user scrolls down 1500px from the top of the document, show the button
+        window.onscroll = function () {
+          scrollFunction();
+        };
         scrollButton.addEventListener("click", topFunction);
       }
     },
